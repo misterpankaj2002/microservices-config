a75b996ec286fee9b2831957151d0795c783e94f
Pankaj Gupta <misterpankaj2002@gmail.com>
2025-10-24 08:28:37 -0400

    Initial commit

---
+++ accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,40 @@
+package com.eazybytes.accounts;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "EazyBank Account microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Pankaj Gupta",
+                        email = "pankaj@gmail.com",
+                        url = "http://pankajgupta.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://pankajgupta.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Account microservice REST API Documentation",
+                url = "http://localhost:8080/swagger-ui/index.html"
+        )
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
---
+++ zzsection6/v1-springboot/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,43 @@
+package com.eazybytes.accounts;
+
+import com.eazybytes.accounts.dto.AccountsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "EazyBank Account microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Pankaj Gupta",
+                        email = "pankaj@gmail.com",
+                        url = "http://pankajgupta.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://pankajgupta.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Account microservice REST API Documentation",
+                url = "http://localhost:8080/swagger-ui/index.html"
+        )
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
